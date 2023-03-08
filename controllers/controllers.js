ea26785e429bff0a3282f064d97f89467f3b7de8
mwongess <amosmwongelah@gmail.com>
2023-03-08 19:19:59 +0300

    Do Not Push Your PASSWORD

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,17 +4,13 @@ const getBlogs = (req, res) => {
   try {
     const query = "SELECT * FROM blogs";
 
-    connection.query(
-      query,
-      (
-      (error, blog) => {
-        if (error) {
-          res.json(error);
-          return;
-        }
-        res.json(blog);
-      })
-    );
+    connection.query(query, (error, blogs) => {
+      if (error) {
+        res.json(error);
+        return;
+      }
+      res.json(blogs);
+    });
   } catch (error) {
     res.json(error);
   }
@@ -24,17 +20,13 @@ const getBlog = (req, res) => {
   try {
     const id = req.params.id;
     const query = `SELECT * FROM blogs WHERE id="${id}"`;
-    connection.query(
-      query,
-      (
-      (error, blog) => {
-        if (error) {
-          res.json(error);
-          return;
-        }
-        res.json(blog);
-      })
-    );
+    connection.query(query, (error, blog) => {
+      if (error) {
+        res.json(error);
+        return;
+      }
+      res.json(blog);
+    });
   } catch (error) {
     res.json(error);
   }
@@ -43,37 +35,29 @@ const createBlog = (req, res) => {
   try {
     const { id, title, content } = req.body;
     const query = `INSERT INTO blogs VALUES("${id}","${title}","${content}" )`;
-    connection.query(
-      query,
-      (
-      (error, blog) => {
-        if (error) {
-          res.json(error);
-          return;
-        }
-        res.json(blog);
-      })
-    );
+    connection.query(query, (error, info) => {
+      if (error) {
+        res.json(error);
+        return;
+      }
+      res.json(info);
+    });
   } catch (error) {
     res.json(error);
   }
 };
 const updateBlog = (req, res) => {
   try {
-    const {title , content} = req.body
+    const { title, content } = req.body;
     const id = req.params.id;
     const query = `UPDATE blogs SET title="${title}", content="${content}" WHERE id="${id}"`;
-    connection.query(
-      query,
-      (
-      (error,blog) => {
-        if (error) {
-          res.json(error);
-          return;
-        }
-        res.json(blog);
-      })
-    );
+    connection.query(query, (error, info) => {
+      if (error) {
+        res.json(error);
+        return;
+      }
+      res.json(info);
+    });
   } catch (error) {
     res.json(error);
   }
@@ -82,17 +66,13 @@ const deleteBlog = (req, res) => {
   try {
     const id = req.params.id;
     const query = `DELETE FROM blogs WHERE id="${id}"`;
-    connection.query(
-      query,
-      (
-      (error, blog) => {
-        if (error) {
-          res.json(error);
-          return;
-        }
-        res.json(blog);
-      })
-    );
+    connection.query(query, (error, info) => {
+      if (error) {
+        res.json(error);
+        return;
+      }
+      res.json(info);
+    });
   } catch (error) {
     res.json(error);
   }
